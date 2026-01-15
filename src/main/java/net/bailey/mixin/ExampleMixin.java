9cd9b0ef1233712a419d2a293a9a4432d89b8968
Baileyy4 <btjgames51@gmail.com>
2026-01-15 22:10:21 +0000

    ELP

--- src/main/java/net/bailey/mixin/ExampleMixin.java
+++ src/main/java/net/bailey/mixin/ExampleMixin.java
@@ -8,8 +8,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftServer.class)
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "loadLevel")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadLevel()V
+    @Inject(method = "createWorlds", at = @At("HEAD"))
+    private void onCreateWorlds(CallbackInfo info) {
+        // Runs when worlds are created
 	}
 }
